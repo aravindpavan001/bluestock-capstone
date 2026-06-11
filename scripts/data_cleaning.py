6c2b67219cf74ac66d28c83863856f2e414e02d5
Repalle Aravind Pavan Kumar <repallepavan303@gmail.com>
2026-06-11 21:49:36 +0530

    Final: Complete Bluestock MF Capstone

---
+++ scripts/data_cleaning.py
@@ -0,0 +1,37 @@
+"""
+Cleans and preprocesses raw mutual fund dataset.
+
+Author: Repalle Aravind Pavan Kumar
+Project: Bluestock Mutual Fund Capstone
+"""
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("data/raw/02_nav_history.csv")
+
+print("Original Shape:", df.shape)
+
+# Convert date column
+df['date'] = pd.to_datetime(df['date'])
+
+# Sort values
+df = df.sort_values(by=['amfi_code', 'date'])
+
+# Convert NAV to numeric
+df['nav'] = pd.to_numeric(df['nav'], errors='coerce')
+
+# Forward fill missing NAV values
+df['nav'] = df.groupby('amfi_code')['nav'].ffill()
+
+# Remove duplicates
+df = df.drop_duplicates()
+
+# Keep only valid NAV values
+df = df[df['nav'] > 0]
+
+print("Cleaned Shape:", df.shape)
+
+# Save cleaned dataset
+df.to_csv("data/processed/nav_history_cleaned.csv", index=False)
+
+print("Cleaned NAV history saved successfully!")
